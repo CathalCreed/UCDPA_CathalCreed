bfabcadc7ccc9574d19fe886ebcaa7d07ed71314
Cathal Creed <2013cathalcreed@gmail.com>
2021-11-16 17:39:01 +0000

    'second commit'

--- DataSet.py
+++ DataSet.py
@@ -1,19 +1,39 @@
 import pandas as pd
+import numpy as np
 
 Suicide_data= pd.read_csv(r'C:\Users\Brian Creed\Downloads\master.csv.zip')
 
 Ireland_suicide_data = Suicide_data[Suicide_data['country'] == 'Ireland']
 
-Ireland_suicide_data.set_index('year')
+Ireland_suicide_data.isnull().sum()
+full_Ireland_suicide_data = Ireland_suicide_data.fillna(method = 'ffill')
 
-Ireland_suicide_data.sort_values(by = 'year')
+full_Ireland_suicide_data.set_index('year')
 
-From_1985_to_1989 = Ireland_suicide_data[0:60]
-From_1990_to_1994 = Ireland_suicide_data[60:120]
-From_1995_to_1999 = Ireland_suicide_data[120:180]
-From_2000_to_2004 = Ireland_suicide_data[180:240]
-From_2005_to_2009 = Ireland_suicide_data[240:300]
-From_2010_to_2014 = Ireland_suicide_data[300:360]
+full_Ireland_suicide_data.sort_values(by = 'year')
+
+From_1985_to_1989 = full_Ireland_suicide_data[0:60]
+From_1990_to_1994 = full_Ireland_suicide_data[60:120]
+From_1995_to_1999 = full_Ireland_suicide_data[120:180]
+From_2000_to_2004 = full_Ireland_suicide_data[180:240]
+From_2005_to_2009 = full_Ireland_suicide_data[240:300]
+From_2010_to_2014 = full_Ireland_suicide_data[300:360]
+
+
+def total_suicides(n):
+    total = np.sum(n['suicides_no'])
+    return total
+
+plot_point1 = total_suicides(From_1985_to_1989)
+plot_point2 = total_suicides(From_1990_to_1994)
+plot_point3 = total_suicides(From_1995_to_1999)
+plot_point4 = total_suicides(From_2000_to_2004)
+plot_point5 = total_suicides(From_2005_to_2009)
+plot_point6 = total_suicides(From_2010_to_2014)
+
+gender = From_1985_to_1989.groupby('sex').sum()
+
+
 
 
 
